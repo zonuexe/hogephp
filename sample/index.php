7faee35c8db460f5deb54f5c0788825828197aa7
USAMI Kenta <tadsan@zonu.me>
2020-01-22 23:54:51 +0900

    Add sample code

---
+++ sample/index.php
@@ -0,0 +1,9 @@
+<?= (require __DIR__ . '/../Hoge.php')(function (Hoge\Framework $php) {
+    $php->get('/', [], function () { ?>
+<!DOCTYPE html>
+<html>
+<title>Hoge index</title>
+<h1>The Hoge PHP Framework</h1>
+</html>
+    <?php });
+}) ?>
